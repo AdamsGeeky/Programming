90d6214935ca9d0bb3cf2c90e7f60b05dc465aca
ADAMU MUHAMMAD MUHAMMAD <muhdadamu0008@gmail.com>
2024-05-29 22:39:45 +0100

    feat: input outup

---
+++ C++/IO/src/include/input.cpp
@@ -0,0 +1,23 @@
+/**
+ * Author: Adamu Muhammad Muhammad
+ * Date: 2024-05-29
+ * Description: Input output in C++
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Welcome to C++" << endl;
+  cout << "Enter your name: ";
+  string name;
+  cin >> name;
+  cout << "Hello, " << name << "!" << endl;
+  cout << "How are you today?" << endl;
+  string answer;
+  cin >> answer;
+  cout << "I'm glad to hear that!" << endl;
+  cout << "Bye!" << endl;
+  return 0;
+}
---
+++ C++/IO/src/include/output.cpp
@@ -0,0 +1,14 @@
+/**
+ * Author: Adamu Muhammad Muhammad
+ * Date: 2024-05-29
+ * Description: Input output in C++
+ */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Welcome, World!" << endl;
+
+  return 0;
+}
